ef424c431302487713ae9c6c03d27cc3485b4e48
Affeeq <affeeqabedeenismail@gmail.com>
2020-05-08 17:07:11 -0400

    move VueRouter instance into routes.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router';
 import App from './App.vue'
-import { routes } from './routes';
+import router from './routes';
 import { store } from './store/store.js';
-
-// app config
-Vue.use(VueRouter);
-
-// accessing routes
-const router = new VueRouter({
-	routes,
-	mode: 'history'
-});
-
 
 // root vue instance
 new Vue({
