0e42a2e5a8b0b74508cdbcd7ed4c65f0b5374dea
Affeeq <affeeqabedeenismail@gmail.com>
2020-05-08 08:38:06 -0400

    Move the link into a module

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
 import App from './App.vue'
 import { routes } from './routes';
 import { store } from './store/store.js';
 
 // app config
 Vue.use(VueRouter);
-Vue.use(VueResource);
-
-// root link to DB
-Vue.http.options.root = 'https://the-stock-trader-6732a.firebaseio.com/';
 
 // accessing routes
 const router = new VueRouter({
