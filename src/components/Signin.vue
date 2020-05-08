9f9bae739c81875fefc40e4b07f5f63236756752
Affeeq <affeeqabedeenismail@gmail.com>
2020-05-08 08:41:01 -0400

    Add components for signin and signup page. Added action to make api call to firebase

---
+++ src/components/Signin.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="container">
+		<div class="container">
+		<h1 class="text-center">Sign In</h1>
+		<form>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model="email">
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" class="form-control" id="password" v-model="password">
+			</div>
+			<button type="submit" class="btn btn-primary" @click.prevent="signIn({ email, password })">Submit</button>
+		</form>
+	</div>
+	</div>
+</template>
+
+<script>
+	import { mapActions } from 'vuex';
+
+	export default {
+		data() {
+			return {
+				email: '',
+				password: ''
+			}
+		},
+		methods: {
+			...mapActions('data', [
+				'signIn'
+			])
+		}
+	}
+</script>
+
+<style></style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="container">
+		<h1 class="text-center">Sign Up</h1>
+		<form>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input type="email" class="form-control" id="email" aria-describedby="emailHelp" v-model="email">
+			</div>
+			<div class="form-group">
+				<label for="confirmEmail">Confirm Email</label>
+				<input type="email" class="form-control" id="confirmEmail" aria-describedby="emailHelp">
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" class="form-control" id="password" v-model="password">
+			</div>
+			<div class="form-group">
+				<label for="confirmPassword">Confirm Password</label>
+				<input type="password" class="form-control" id="confirmPassword">
+			</div>
+			<button type="submit" class="btn btn-primary" @click.prevent="signUp({email, password})">Submit</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	import { mapActions } from 'vuex';
+
+	export default {
+		data() {
+			return {
+				email: '',
+				password: ''
+			}
+		},
+		methods: {
+			...mapActions('data', [
+				'signUp'
+			])
+		}
+	}
+</script>
+
+<style></style>
