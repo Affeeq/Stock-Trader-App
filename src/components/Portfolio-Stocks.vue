f4fd32328783db38366981c07749f2928baae673
Affeeq <affeeqabedeenismail@gmail.com>
2020-05-19 12:02:37 -0400

    Add Portfolio-Stocks.vue for portfolio stocks. Solve validation error using parseInt

---
+++ src/components/Portfolio-Stocks.vue
@@ -0,0 +1,79 @@
+<template>
+	<b-card no-body>
+		<b-card-header header-bg-variant="primary">
+			<b-card-title title-tag="h5">{{ portfolioStock.name }}</b-card-title>
+			<p>(Price: {{ portfolioStock.price }} | Quantity: {{ portfolioStock.quantity }} )</p>
+		</b-card-header>
+		<b-card-body class="text-center text-md-left">
+			<input 
+			type="number" 
+			placeholder="Quantity" 
+			v-model="quantity" 
+			@blur="clear(quantity, index)"
+			:class="{ danger: disable(quantity, portfolioStock.quantity) }"
+			>
+			<b-button
+			variant="danger"
+			class="float-md-right mt-3 mt-md-0" 
+			@click="sellStocks({portfolioStock, index})"
+			:disabled="disable(quantity, portfolioStock.quantity)"
+			>Sell</b-button>
+		</b-card-body>
+	</b-card>
+</template>
+
+<script>
+	import { mapActions } from 'vuex';
+	export default {
+		data() {
+			return {
+				quantity: ''
+			}
+		},
+		props: ['portfolioStock', 'index'],
+		methods: {
+			...mapActions('portfolio', [
+				'sellStocks'
+			]),
+			...mapActions('custom', [
+				'currentQuantity'
+			]),
+			// need to check for danger styling for quantity
+			disable(userQuantity, portfolioQuantity) {
+				// console.log(parseInt(userQuantity) > parseInt(portfolioQuantity))
+				// console.log(portfolioQuantity)
+				// console.log(userQuantity)
+
+				return userQuantity <= 0 || parseInt(userQuantity) > parseInt(portfolioQuantity) || (userQuantity - Math.floor(userQuantity)) !== 0;
+			},
+			clear(quantity,index) {
+				this.currentQuantity(quantity);
+				let vm = this;
+				setTimeout(function() { vm.quantity = '' }, 200);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.card-title {
+		margin: 0;
+		display: inline;
+	}
+
+	.card-title + p {
+		display: inline;
+	}
+
+	.danger:focus {
+		outline: none !important;
+		border:1px solid red;
+		box-shadow: 0 0 10px red;
+	}
+
+	@media (max-width: 600px) {
+		input {
+			width: 75%;
+		}
+	}
+</style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,81 +1,26 @@
 <template>
 	<b-container>
 		<b-row>
-			<b-col cols="6" class="mt-3" v-for="(pf, index) in portfolio">
-				<b-card no-body>
-					<b-card-header header-bg-variant="primary">
-						<b-card-title title-tag="h5">{{ pf.name }}</b-card-title>
-						<p>(Price: {{ pf.price }} | Quantity: {{ pf.quantity }} )</p>
-					</b-card-header>
-					<b-card-body class="text-center text-md-left">
-						<input 
-							type="number" 
-							placeholder="Quantity" 
-							v-model="quantity[index]" 
-							@blur="clear(quantity[index], index)"
-							:class="{ danger: disable(quantity[index], pf.quantity) }"
-						>
-						<b-button
-							variant="danger"
-							class="float-md-right mt-3 mt-md-0" 
-							@click="sellStocks({pf, index})"
-							:disabled="disable(quantity[index], pf.quantity)"
-						>Sell</b-button>
-					</b-card-body>
-				</b-card>
+			<b-col cols="6" class="mt-3" v-for="(portfolioStock, index) in portfolio">
+				<app-portfolio-stocks :portfolioStock="portfolioStock" :index="index"></app-portfolio-stocks>
 			</b-col>
 		</b-row>
 	</b-container>
 </template>
 
 <script>
-	import { mapActions } from 'vuex';
+	import PortfolioStocks from './Portfolio-Stocks.vue';
 	export default {
 		data() {
 			return {
-				portfolio: this.$store.state.portfolio.portfolio,
-				quantity: []
+				portfolio: this.$store.state.portfolio.portfolio
 			}
 		},
-		methods: {
-			...mapActions('portfolio', [
-				'sellStocks'
-			]),
-			...mapActions('custom', [
-				'currentQuantity'
-			]),
-			// need to check for danger styling for quantity
-			disable(quantity, portfolioQuantity) {
-				return quantity <= 0 || quantity > portfolioQuantity || (quantity - Math.floor(quantity)) !== 0;
-			},
-			clear(quantity,index) {
-				this.currentQuantity(quantity);
-				let vm = this;
-				setTimeout(function() { vm.quantity.splice(index, 1, '') }, 200);
-			}
+		components: {
+			appPortfolioStocks: PortfolioStocks
 		}
 	}
 </script>
 
-<style scoped>
-	.card-title {
-		margin: 0;
-		display: inline;
-	}
-
-	.card-title + p {
-		display: inline;
-	}
-
-	.danger:focus {
-		outline: none !important;
-		border:1px solid red;
-		box-shadow: 0 0 10px red;
-	}
-
-	@media (max-width: 600px) {
-		input {
-			width: 75%;
-		}
-	}
+<style>
 </style>
