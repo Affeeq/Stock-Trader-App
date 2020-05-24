3f5d07cf26d5999edc26f01dc686ecd8d0be1c33
Affeeq <affeeqabedeenismail@gmail.com>
2020-05-23 20:26:45 -0400

    Solve selling stocks issue with payload change of name

--- src/store/modules/custom.js
+++ src/store/modules/custom.js
@@ -14,7 +14,6 @@ const mutations = {
 		state.funds = payload.funds;
 	},
 	currentQuantity: (state, payload) => {
-		console.log(payload)
 		state.currentQuantity = payload;
 	},
 	minusFunds: (state, payload) => {
